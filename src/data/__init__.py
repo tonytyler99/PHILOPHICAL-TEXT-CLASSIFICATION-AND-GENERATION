bbd181065e7eeb162b8ce4a94cf24ba67a9e0b62
Berkin Kabaağaç <87212629+tonytyler99@users.noreply.github.com>
2026-02-26 10:07:47 +0300

    Import TextPreprocessor and prepare_classification_data

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 """Data module for PHIL-TEXT: loading and processing philosophy texts."""
 from src.data.load_data import load_data, load_corpus, get_data_summary
+from src.data.preprocess import TextPreprocessor, prepare_classification_data
 
